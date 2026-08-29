b694c0cf2ebd451faca5474666ac3e9130d32657
devthigas502 <devthigas502@users.noreply.github.com>
2026-08-29 14:39:34 -0300

    fix(deploy): use ruby schema format and rails db:migrate for postgres compatibility

--- config/application.rb
+++ config/application.rb
@@ -37,8 +37,6 @@ module AgendaAi
     config.i18n.default_locale = :"pt-BR"
     config.i18n.available_locales = [:"pt-BR", :en]
 
-    config.active_record.schema_format = :sql
-
     # Use UUIDs as primary keys by default
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
