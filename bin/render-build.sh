b617e09633a254144429ba897ac4fddce495e237
devthigas502 <devthigas502@users.noreply.github.com>
2026-08-29 14:37:02 -0300

    fix(deploy): use SECRET_KEY_BASE_DUMMY for assets precompilation in render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
+SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:prepare
